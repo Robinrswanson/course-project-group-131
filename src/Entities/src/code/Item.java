040fd25f6c9ed6084a9c2880845326d7fe32ee8d
tonyxieuoft <xiaohan.xie@mail.utoronto.ca>
2022-10-31 20:05:58 -0400

    Deleted the default modules (HelloWorld main and test), created the Entities folder, and made the backbone of the Item class

---
+++ src/Entities/src/code/Item.java
@@ -0,0 +1,32 @@
+package code;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Date;
+
+public class Item {
+
+    private int quantity;
+    private double price;
+    private Date expirationDate;
+
+    private String name;
+    private String storageLocation;
+
+    private List<String> categories = new ArrayList<>();
+
+    public Item(String name, double price, String storageLocation){
+        // very basic constructor, missing a lot of functionality
+        // implementation dependent on the person working on the "add" use case
+        quantity = 0;
+        this.price = price;
+        this.name = name;
+        this.storageLocation = storageLocation;
+    }
+
+    public boolean isCategory(String category){
+        return true;
+    }
+
+
+}
