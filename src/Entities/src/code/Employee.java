aed7c63310602cccf00e4a172459b24069cdb7fd
Robin <robinr.swanson@mail.utoronto.ca>
2022-11-14 19:37:17 -0500

    Merged main into branch

---
+++ src/Entities/src/code/Employee.java
@@ -0,0 +1,12 @@
+package entities.src.code;
+
+import java.util.*;
+
+public class Employee {
+    private String username;
+    private String password;
+
+    public void updateitem(){
+
+    }
+}
