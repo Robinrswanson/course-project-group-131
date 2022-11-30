893735c96418e68ecb58a768c61135ce11ed6633
Robinrswanson <robinr.swanson@mail.utoronto.ca>
2022-11-29 23:06:17 -0500

    Renamed Files

---
+++ src/main/java/interface_adaptors/GatewayReader.java
@@ -0,0 +1,31 @@
+package interface_adaptors;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class gatewayReader {
+
+    private final File file;
+
+    public gatewayReader(String filePath){
+        this.file = new File(filePath);
+    }
+
+    public gatewayReader(File file){
+        this.file = file;
+    }
+    public List<String[]> getData() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        List<String[]> fileData = new ArrayList<>();
+        String st;
+        while ((st = reader.readLine()) != null){
+            String[] rowData = st.split(",");
+            fileData.add(rowData);
+        }
+        return fileData;
+    }
+
+
+}
---
+++ src/main/java/interface_adaptors/GatewayWriter.java
@@ -0,0 +1,55 @@
+package interface_adaptors;
+
+import java.io.*;
+import java.util.List;
+
+public class gatewayWriter {
+    private final File file;
+
+
+    public gatewayWriter(String filePath) {
+        this.file = new File(filePath);
+    }
+
+    public void addNewLines(String[] rowData) throws IOException {
+
+    }
+
+    public void editSingleCell(String replace, int row, int col) throws IOException {
+        try {
+            // could possibly change row and col to search for item name
+            gatewayReader reader = new gatewayReader(file);
+            List<String[]> fileContents = reader.getData();
+            // contents to be updated are searched using row and col in csv file
+            fileContents.get(row)[col] = replace;
+            // entire file is rewritten as there is no replace method for CSVWriter
+            FileWriter fileWriter = new FileWriter(file);
+            PrintWriter pw = new PrintWriter(fileWriter);
+            for (String[] rowData : fileContents) {
+                String line = String.join(",", rowData);
+                pw.println(line);
+            }
+            pw.flush();
+            pw.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    public void rewriteFile(List<String[]> newFileContents) {
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            PrintWriter pw = new PrintWriter(fileWriter);
+            for (String[] rowData : newFileContents) {
+                String line = String.join(",", rowData);
+                pw.println(line);
+            }
+            pw.flush();
+            pw.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    //Add another method that allows multiple changes to be made
+//  Could use same writeToFile method but instead pass in arrays
+
+}
