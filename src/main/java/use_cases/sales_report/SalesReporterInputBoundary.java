c0c98334cdf72b7b3e92668c5470316a97219ae0
tofu-lab <lester.kong@mail.utoronto.ca>
2022-12-05 20:13:45 -0500

    Extracted methods from SalesReporter into SalesReporterInputData to adhere better to clean architecture.

--- src/main/java/use_cases/sales_report/SalesReporterInputBoundary.java
+++ src/main/java/use_cases/sales_report/SalesReporterInputBoundary.java
@@ -3,9 +3,7 @@ package use_cases.sales_report;
 public interface SalesReporterInputBoundary {
     /**
      * Generates a sales report with data in an external History file
-     * @param rows is the data that is read from an external History file
-     * @param startTime is the start time of the sales report
-     * @param endTime is the end time of the sales report
+     * @param data contains the rows of data from the external History file along with a user input startTime and endTime
      */
     void generateReport(SalesReporterInputData data);
 }
