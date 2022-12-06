472d4977b201f61836534f79be0794f1b24f298f
Daisy Li <daisysiyi.li@mail.utoronto.ca>
2022-12-06 00:12:09 -0500

    Since someone merged their codes, updated and committed changes.

--- src/main/java/use_cases/gateway_interfaces/GatewayReaderInterface.java
+++ src/main/java/use_cases/gateway_interfaces/GatewayReaderInterface.java
@@ -6,9 +6,9 @@ import java.util.List;
 public interface GatewayReaderInterface {
 
     String getFilePath();
-    void setFilePath(String filePath);
     List<String[]> getData() throws IOException;
 
+    void setFilePath(String filePath);
 
 
 }
