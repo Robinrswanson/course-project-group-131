719d8375d1dd0c9cad8991b4a30ca6821c3cda96
tofu-lab <lester.kong@mail.utoronto.ca>
2022-12-05 22:57:02 -0500

    ???

--- src/main/java/use_cases/gateway_interfaces/GatewayReaderInterface.java
+++ src/main/java/use_cases/gateway_interfaces/GatewayReaderInterface.java
@@ -6,6 +6,7 @@ import java.util.List;
 public interface GatewayReaderInterface {
 
     String getFilePath();
+    void setFilePath(String filePath);
     List<String[]> getData() throws IOException;
 
 
