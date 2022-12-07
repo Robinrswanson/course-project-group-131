48a84bc2deebe66fc9aa063d0f310dce7ebf27f9
Navkiran Gill <navkiran.gill@mail.utoronto.ca>
2022-12-06 19:33:58 -0500

    Added ReturnFeatureBuilder

---
+++ src/main/java/main_builders_directors/ReturnFeatureBuilder.java
@@ -0,0 +1,54 @@
+package main_builders_directors;
+
+import interface_adaptors.arr.ARRIView;
+import interface_adaptors.arr.ReturnController;
+import interface_adaptors.arr.ReturnPresenter;
+import screens.ReturnScreen;
+import use_cases.arr.ARRInputBoundary;
+import use_cases.arr.ARROutputBoundary;
+import use_cases.arr.ReturnUseCase;
+
+import javax.swing.*;
+
+public class ReturnFeatureBuilder implements FeatureBuilder{
+
+    private ARRIView returnScreen;
+    private ARRInputBoundary returnUseCase;
+    private ReturnController returnController;
+    private ARROutputBoundary returnPresenter;
+
+    public void buildPresenter()
+    {
+        returnPresenter = new ReturnPresenter();
+    }
+
+    public void buildUseCase()
+    {
+        returnUseCase = new ReturnUseCase(returnPresenter);
+    }
+
+    public void buildController()
+    {
+        returnController = new ReturnController(returnUseCase);
+    }
+
+    public void buildScreen(JPanel Screens)
+    {
+        returnScreen = new ReturnScreen(Screens, returnController);
+    }
+
+    public void presenterSetScreen()
+    {
+        returnPresenter.setScreen(returnScreen);
+    }
+
+    public JPanel getScreen()
+    {
+        return (JPanel)returnScreen;
+    }
+
+    public String getScreenName()
+    {
+        return ARRIView.RETURN_SCREEN_NAME_CONSTANT;
+    }
+}
