28e7b699bf9f69d3c5394e9f65a59f64b396bced
tonyxieuoft <xiaohan.xie@mail.utoronto.ca>
2022-12-06 20:34:40 -0500

    Added clean architecture for the Login Use Case

--- src/main/java/screens/LoginScreen.java
+++ src/main/java/screens/LoginScreen.java
@@ -1,6 +1,8 @@
 package screens;
 
-import use_cases.login.LoginCheckUserExists;
+import interface_adaptors.login.LoginController;
+import interface_adaptors.login.LoginIView;
+import use_cases.login.Login;
 import use_cases.login.LoginOutPutDS;
 
 import javax.imageio.ImageIO;
@@ -14,7 +16,7 @@ import java.io.IOException;
 import java.util.List;
 
 
-public class LoginScreen extends JPanel implements ActionListener {
+public class LoginScreen extends JPanel implements ActionListener, LoginIView {
 
 
     private static JPanel panel;
@@ -25,45 +27,71 @@ public class LoginScreen extends JPanel implements ActionListener {
     private static JPasswordField passwordField;
     private static JButton button;
     private static JLabel picLabel;
+    private static LoginController controller;
 
+    public LoginScreen(JPanel screens, LoginController controller) throws IOException {
 
+        this.panel = screens;
+        this.controller = controller;
 
-    public LoginScreen(JPanel screens) throws IOException {
-
-
-        panel = screens;
         this.setLayout(null);
-        pwlabel = new JLabel("Password");
-        userlabel = new JLabel("Username");
+
+        setPicture();
+
+        setPasswordLabel();
+        setUserNameLabel();
+
+        setUsernameField();
+        setPasswordField();
+
+        setButton();
+
+        setNotification();
+
+    }
+
+    private void setNotification() {
         notification_label = new JLabel("");
         notification_label.setBounds(150,185,165,25);
+        this.add(notification_label);
+    }
 
-        JPanel panel2 = new JPanel();
-
-        pwlabel.setBounds(60, 160, 80, 25);
-        userlabel.setBounds(60, 120, 80, 25);
-        usertextField = new JTextField(20);
-        usertextField.setBounds(140, 120, 165, 25);
-        passwordField = new JPasswordField();
-        passwordField.setBounds(140, 160, 165, 25);
+    private void setButton() {
         button = new JButton("Login");
         button.setBounds(160, 210, 80, 25);
         button.addActionListener(this);
+        this.add(button);
+    }
 
+    private void setPasswordField() {
+        passwordField = new JPasswordField();
+        passwordField.setBounds(140, 160, 165, 25);
+        this.add(passwordField);
+    }
+
+    private void setUsernameField() {
+        usertextField = new JTextField(20);
+        usertextField.setBounds(140, 120, 165, 25);
+        this.add(usertextField);
+    }
+
+    private void setUserNameLabel() {
+        userlabel = new JLabel("Username");
+        userlabel.setBounds(60, 120, 80, 25);
+        this.add(userlabel);
+    }
+
+    private void setPasswordLabel() {
+        pwlabel = new JLabel("Password");
+        pwlabel.setBounds(60, 160, 80, 25);
+        this.add(pwlabel);
+    }
+
+    private void setPicture() throws IOException {
         BufferedImage myPicture = ImageIO.read(new File("src/main/java/database/Inventory Icon.png"));
         picLabel = new JLabel(new ImageIcon(myPicture));
         picLabel.setBounds(160,10,100,96);
-
         this.add(picLabel);
-        this.add(pwlabel);
-        this.add(userlabel);
-        this.add(usertextField);
-        this.add(passwordField);
-        this.add(button);
-        this.add(notification_label);
-
-
-
     }
 
 
@@ -75,46 +103,28 @@ public class LoginScreen extends JPanel implements ActionListener {
         // determine 1.
         //If the username exists, 2. if the password matches 3. If the user is a manager or employee. and returns
         //this result as a boolean list. If password matches and username exists, a new Employee screen is created.
-        String username = usertextField.getText();
-        char[] password = passwordField.getPassword();
-        StringBuilder pwBuilder = new StringBuilder();
-        for (char i : password) {
-            pwBuilder.append(i);
-        }
-
-        String pwFinal = pwBuilder.toString();
-        LoginCheckUserExists check = new LoginCheckUserExists(username, pwFinal);
-        List<Boolean> Bool_List = check.CheckExists(check.username, check.password);
-
-
-        if (Bool_List.size() == 1) {
-            notification_label.setForeground(Color.RED);
-            notification_label.setText("Username not found");
-        } else if (Bool_List.size() == 2) {
-            notification_label.setForeground(Color.RED);
-            notification_label.setText("Password does not match");
-
-        } else if (Bool_List.size() == 3) {
+        controller.checkUserExists(usertextField.getText(), passwordField.getPassword());
 
             //Stores Is_Manager and username in static variables of LoginOutPutDS class
-
-            LoginOutPutDS ds = new LoginOutPutDS();
-            ds.Set_Is_Manager(Bool_List.get(2));
-            ds.Set_username(username);
 
 //            LoginOutPutDS ds2 = new LoginOutPutDS();
 //            System.out.println(ds2.Get_Is_Manager());
 //            System.out.println(ds2.Get_username());
-            CardLayout cardLayout = (CardLayout) panel.getLayout();
-            cardLayout.show(panel, "Main");
+    }
 
+    public void displayPasswordFail() {
+        notification_label.setForeground(Color.RED);
+        notification_label.setText("Password does not match");
+    }
 
+    public void displayUsernameFail() {
+        notification_label.setForeground(Color.RED);
+        notification_label.setText("Username not found");
+    }
 
-
-
-
-        }
-
+    public void changeToMainScreen() {
+        CardLayout cardLayout = (CardLayout) panel.getLayout();
+        cardLayout.show(panel, "Main");
     }
 }
 
