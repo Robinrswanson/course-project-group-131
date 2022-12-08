f7f76ded7d9b4e38d11c71cb0e3faaf2e187c3c7
Jackson-Tsang <104661959+Jackson-Tsang@users.noreply.github.com>
2022-12-08 01:19:12 -0500

    Fixed problems with Github

--- src/main/java/entities/SortStrategy.java
+++
@@ -1,8 +0,0 @@
-package entities;
-
-public interface SortStrategy {
-    // This strategy for all the different types of sort, as strategy allows for a switch at runtime of the
-    // algorithm used, so you can sort though multiple different attributes at once.
-    void sort();
-
-}
